aead7e1d9d781328ecfa9d0bc23638bf4dd18697
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-09-22 23:16:21 -0300

    Aula 02 - CRUD NodeJS w/ BDA Prisma

--- aula02/src/app.module.ts
+++ aula02/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
   imports: [ProductModule],
 })
 export class AppModule {}
--- aula02/src/product/product.service.ts
+++ aula02/src/product/product.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
+  constructor (private readonly prisma: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
   findAll() {
-    return `This action returns all product`;
+    // return `This action returns all product`;
+    return this.prisma.product.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    // return `This action returns a #${id} product`;
+    return this.prisma.product.findUnique(
+      {
+        where: {id}
+      });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
